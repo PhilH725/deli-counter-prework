fb799ecf98ca61fa11a12821456a38c6aec44ef2
Phil Hartley <philh725@gmail.com>
2018-10-31 20:25:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+def line(current_line)
+  if current_line.size > 0 
+    "The line is currently empty."
+  else
+    for i in (1..current_line.size)
+      "You are #{i} in line."
+end
