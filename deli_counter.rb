c231b3bead58597de503a854e93da9820a78ce42
Phil Hartley <philh725@gmail.com>
2018-10-31 20:30:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,10 @@ def line(current_line)
   if current_line.size == 0 
     puts "The line is currently empty."
   else
+    message = "The line is currently: "
     for i in (1..current_line.size)
-      "You are #{i} in line."
+      message += "#{i}. #{current_line[i - 1]}."
     end
+    puts message
   end
 end
