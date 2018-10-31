f97442f75c4503ffc38e2975dfddf503ea38fcdc
Phil Hartley <philh725@gmail.com>
2018-10-31 20:25:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(current_line)
-  if current_line.size > 0 
+  if current_line.size == 0 
     "The line is currently empty."
   else
     for i in (1..current_line.size)
